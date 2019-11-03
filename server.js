f991f71a561d4c942e4f119d5c91760236f08e5f
volkan ongun <me@volkanongun.com>
2019-11-03 12:36:16 +0300

    initial working server and client

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/frontend'));
+app.use('/three',express.static(__dirname + '/node_modules/three/build/'));
+app.use('/io',express.static(__dirname + '/node_modules/socket.io-client/dist/'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
